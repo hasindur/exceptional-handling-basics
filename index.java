daf44112478260d36fdbd42db331a4c2b4f87e22
Hasindu Ranasinghe <152050830+hasindur@users.noreply.github.com>
2025-05-15 21:18:06 +0530

    excaeption
    because b=0 is not defined.

--- index.java
+++ index.java
@@ -3,9 +3,10 @@ public class index {
     public static void main(String[] args) {
         int a, b, c;
         a = 34;
-        b = 2;
-        c = a / b;
+        b = 0;
+
         System.out.println("Start");
+        c = a / b;
         System.out.println("Answer : " + c);
         System.out.println("End");
     }
